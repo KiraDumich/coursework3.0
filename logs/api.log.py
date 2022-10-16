e004eb4dfb4056c91827b79c96f2f4a13a425382
79244 <dumichkira05@gmail.com>
2022-10-16 23:55:21 +1000

    d

---
+++ logs/api.log.py
@@ -0,0 +1,16 @@
+import logging
+
+# логгеры
+logger_api = logging.getLogger('one')
+
+# обработчик логгера
+logger_handler = logging.StreamHandler()
+
+# форматирование(Formatter)
+formatter_one = logging.Formatter("%(levelname)s : %(asctime)s : %(message)s")
+logger_handler.setFormatter(formatter_one)
+
+# к журналу
+logger_api.addHandler(logger_handler)
+# запись логов
+logging.basicConfig(filename="logs/api.log", level=logging.DEBUG)
