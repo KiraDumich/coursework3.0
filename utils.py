d1802e58b24c352c9e88873b48ce54b598d5812d
79244 <dumichkira05@gmail.com>
2022-11-06 11:27:54 +1000

    ...

--- utils.py
+++ utils.py
@@ -2,8 +2,8 @@
 import json
 
 
-# возвращает пост
 def get_posts_all():
+    """Возвращает пост"""
     try:
         with open('data/posts.json', 'r', encoding='utf-8') as file:
             return json.load(file)
@@ -11,14 +11,14 @@ def get_posts_all():
         print('Error')
 
 
-# возвращает комментарии
 def get_comments_all():
+    """Возвращает комментарии"""
     with open('data/comments.json', 'r', encoding='utf-8') as file:
         return json.load(file)
 
 
-# возвращает посты определенного пользователя.
 def get_posts_by_user(user_name):
+    """Возвращает посты определенного пользователя"""
     posts = get_posts_all()
     posts_user = []
     for post in posts:
@@ -29,8 +29,8 @@ def get_posts_by_user(user_name):
     return posts_user
 
 
-# возвращает комментарии определенного поста
 def get_comments_by_post_id(post_id):
+    """Возвращает комментарии определенного поста"""
     comments_id = []
     comments = get_comments_all()
     for comment in comments:
@@ -39,8 +39,8 @@ def get_comments_by_post_id(post_id):
     return comments_id
 
 
-# считает количество постов
 def get_comments_count(pk):
+    """Считает количество постов"""
     comments = get_comments_all()
     count = 0
     for comment in comments:
@@ -49,8 +49,8 @@ def get_comments_count(pk):
     return count
 
 
-# возвращает список постов по ключевому слову
 def search_for_posts(query):
+    """Возвращает список постов по ключевому слову"""
     posts = get_posts_all()
     posts_query = []
     for post in posts:
@@ -61,8 +61,8 @@ def search_for_posts(query):
     return posts_query
 
 
-# возвращает один пост по его идентификатору.
 def get_post_by_pk(pk):
+    """Возвращает список постов по ключевому слову"""
     posts = get_posts_all()
     for post in posts:
         if pk == post["pk"]:
